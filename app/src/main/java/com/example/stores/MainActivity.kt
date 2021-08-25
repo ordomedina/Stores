be41b9f376661073f2e08b7a396434d829766180
Fco. Javier Ordóñez Medina <ordomedina@gmail.com>
2021-08-25 14:26:57 +0200

    Vídeo 121: 1. Comenzamos con la configuración del recyclerView en MainActivity.

--- app/src/main/java/com/example/stores/MainActivity.kt
+++ app/src/main/java/com/example/stores/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.stores
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.stores.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mAdapter: StoreAdapter
+    private lateinit var mGridlayout: GridLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        mAdapter = StoreAdapter(mutableListOf(), this)
+        mGridlayout = GridLayoutManager(this, 2)
+
+        mBinding.recyclerview.apply {
+            setHasFixedSize(true) // Esta línea es para indicar que tiene un tamaño fijo, ya que le habíamos indicado una altura fija a cada tarjeta.
+            layoutManager = mGridlayout
+            adapter = mAdapter
+        }
+    }
+
+    //OnClicklistener
+    override fun onClick(store: Store) {
+
     }
 }
